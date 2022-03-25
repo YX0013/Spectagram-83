091ffcad93912b777ffd51ca5172d27c64fc9347
YX0013 <83799168+YX0013@users.noreply.github.com>
2022-03-24 20:27:36 -0700

    Add files via upload

---
+++ project-83/screens/Profile.js
@@ -0,0 +1,10 @@
+import * as React from 'react';
+import { Text, View, StyleSheet } from 'react-native';
+
+export default class Profile extends React.Component {
+  render() {
+    return(
+      <Text>Profile Screen</Text>
+    );
+  }
+}
